9ea9e728b5490e9c885ff997778bd04b61a49747
windweller <leo.niecn@gmail.com>
2023-12-05 15:18:36 -0800

    update with an LLM optimizer and push the runnable code for the poem env.

--- autogen/trace/optimizers.py
+++ autogen/trace/optimizers.py
@@ -1,6 +1,8 @@
-
 from autogen.trace.nodes import ParameterNode
 from collections import defaultdict
+from autogen import AssistantAgent
+from textwrap import dedent, indent
+
 
 class Optimizer:
 
@@ -22,6 +24,7 @@ class Optimizer:
             # trace the children of parameters and clean their feedback
             p._feedback = defaultdict(list)
 
+
 class DummyOptimizer(Optimizer):
     # FOR TESTING PURPOSES ONLY
 
@@ -36,8 +39,48 @@ class DummyOptimizer(Optimizer):
             base = value
         else:
             raise NotImplementedError
-        new = base + ' '.join([  ' '.join(l) for l in feedback.values()])
+        new = base + ' '.join([' '.join(l) for l in feedback.values()])
         return new
+
+
+class LLMOptimizer(Optimizer):
+    def __init__(self, parameters, config_list, task_description, *args, **kwargs):
+        super().__init__(parameters, *args, **kwargs)
+        sys_msg = dedent("""
+        You are giving instructions to a student on how to accomplish a task.
+        The student aims to get a high score.
+        Given the feedback the student has received and the instruction you have given,
+        You want to come up with a new instruction that will help the student get a higher score.
+        """)
+        self.llm = AssistantAgent(name="assistant",
+                                  system_message=sys_msg,
+                                  llm_config={"config_list": config_list})
+        self.task_description = task_description
+
+    def _step(self, value, feedback):
+        # `prompt="Complete the following sentence: {prefix}, context={"prefix": "Today I feel"}`
+        # context = {"task_description": self.task_description,
+        #            "instruction": value,
+        #            "feedback": feedback}
+        context = [self.task_description, value['content'], feedback]
+        prompt_space = dedent("""
+        {}
+        
+        This is your instruction to the student from the previous round:
+        {}
+        
+        Using this instruction, this is the feedback the student received:
+        {}
+        
+        Please write down a new instruction to help the student achieve a higher score:
+        """.format(*context))
+
+        messages = [{'content': prompt_space, 'role': 'user'}]
+        response = self.llm.client.create(messages=self.llm._oai_system_message + messages)
+        new_instruct = self.llm.client.extract_text_or_function_call(response)[0]
+        new_node = {'content': new_instruct, 'role': 'system'}
+        return new_node
+
 
 # This is used for propagate
 class FeedbackEnhancer:
--- test/opt/test_poem_opt.py
+++ test/opt/test_poem_opt.py
@@ -9,7 +9,7 @@ from autogen.trace.trace import trace
 from textwrap import dedent, indent
 from env_wrapper import VerbalGymUserAgent
 
-from autogen.trace.optimizers import DummyOptimizer
+from autogen.trace.optimizers import DummyOptimizer, LLMOptimizer
 
 # Load LLM inference endpoints from an env variable or a file
 # See https://microsoft.github.io/autogen/docs/FAQ#set-your-api-endpoints
@@ -19,6 +19,7 @@ config_list = [config_list[1]]
 assert config_list[0]["model"] == "gpt-3.5-turbo-0613"
 
 termination_msg = lambda x: isinstance(x, dict) and "TERMINATE" == str(x.get("content", ""))[-9:].upper()
+
 
 # TODO: like the chessboard, PoemAgent needs to call this before
 class PoemExtractAgent(AssistantAgent):
@@ -32,7 +33,9 @@ class PoemExtractAgent(AssistantAgent):
             is_termination_msg=termination_msg,
         )
 
+
 sys_msg = dedent("You are a student and your teacher gives you an assignment to write a poem.")
+
 
 class StudentAgent(AssistantAgent):
     def __init__(self, seed=1234):
@@ -44,33 +47,58 @@ class StudentAgent(AssistantAgent):
             is_termination_msg=termination_msg,
         )
 
+
 max_turn = 1
 student_agent = trace(StudentAgent)(seed=13)
 user_agent = trace(VerbalGymUserAgent)(env_name="verbal-poem-Haiku-v0",
-                               llm_config={"temperature": 0.0, "config_list": config_list})
+                                       llm_config={"temperature": 0.0, "config_list": config_list})
 
 init_obs = user_agent.get_starting_message()
-user_agent.initiate_chat(student_agent, message=init_obs)
 
 # ======= Now with the env reward, we can optimize =======
 
+optimizer = LLMOptimizer(student_agent.parameters,
+                         config_list=config_list,
+                         task_description=dedent("""
+                         You are helping a student write a poem that satisfies the following requirements:
+                         {}
+                         """.format(init_obs)))  # This just concatenates the feedback into the parameter
 
-optimizer = DummyOptimizer(student_agent.parameters)  # This just concatenates the feedback into the parameter
 
 # use closure to safely add an agent...
 def info_propagate(info_merge_agent=None):
+    # def propagate(child):
+    #     # a dummy function for testing
+    #     summary = ''.join([f'{str(k)}:{v[0]}' for k, v in
+    #                        child.feedback.items()])  # we only take the first feedback for testing purposes
+    #     return {parent: summary for parent in child.parents}
     def propagate(child):
-        # a dummy function for testing
-        summary = ''.join([f'{str(k)}:{v[0]}' for k, v in
-                           child.feedback.items()])  # we only take the first feedback for testing purposes
+        # we only take the actual feedback, no concat
+        summary = ''.join(
+            [v[0] for k, v in child.feedback.items()])  # we only take the first feedback for testing purposes
         return {parent: summary for parent in child.parents}
 
     return propagate
 
+
 propagate = info_propagate(None)
 
-feedback = user_agent.last_message()
+print("Old prompt:", student_agent.parameters[0].data)
+
+user_agent.initiate_chat(student_agent, message=init_obs)
+feedback = user_agent.last_message().data['content']
 last_message = student_agent.last_message()
 optimizer.zero_feedback()
 last_message.backward(feedback, propagate, retain_graph=True)  # Set retain_graph for testing
 optimizer.step()
+
+print("New prompt:", student_agent.parameters[0].data)
+
+user_agent.initiate_chat(student_agent, message=init_obs)
+feedback = user_agent.last_message().data['content']
+last_message = student_agent.last_message()
+optimizer.zero_feedback()
+last_message.backward(feedback, propagate, retain_graph=True)  # Set retain_graph for testing
+optimizer.step()
+
+print("New prompt:", student_agent.parameters[0].data)
