34d3f84f0d43a15c978e5dcc6e1fd0ffdc0cb73c
windweller <leo.niecn@gmail.com>
2024-03-04 11:16:59 -0800

    a full redesign of the optimizer (initial push)

--- autogen/trace/optimizers.py
+++ autogen/trace/optimizers.py
@@ -5,11 +5,13 @@ from autogen.oai.completion import Completion
 from textwrap import dedent, indent
 from copy import copy
 from autogen.trace.propagators import get_label, get_name
+from dataclasses import dataclass
 
 """
 We follow the same design principle as trace
 This file is not dependent of AutoGen library and can be used independently with trace
 """
+
 
 class Optimizer:
 
@@ -116,10 +118,207 @@ class LLMOptimizer(Optimizer):
                 p._feedback = self.parameter_copy[p]["_feedback"]
 
 
-def process_dot_str(dot_str, shrink=True):
-    """
+@dataclass
+class Block:
+    name: str
+    output: str
+    input_llms: list
+    output_llms: list
+    inputs: list
 
-    Args:
+    def __str__(self):
+        return ""
+
+    def __eq__(self, other):
+        # just check the LLM name
+        # and content
+        return self.name == other.name and self.output == other.output
+
+
+def construct_block(block_str: str):
+    input_llm, output_llm = "", ""
+    llm_name_to_cnt = {}
+
+    llm_name_continued = None
+    for line in block_str.split("\n"):
+        if "->" in line:
+            input_llm, output_llm = line.split(" -> ")
+            input_llm, output_llm = input_llm.strip("\""), output_llm.strip("\"")
+            continue
+        if "label=" in line:
+            name = line.split(" [label=")[0].strip().strip("\"")
+            llm_name_to_cnt[name] = ""
+            llm_name_continued = name
+            continue
+
+        llm_name_to_cnt[llm_name_continued] += line.rstrip("\"]")
+
+    return Block(name=output_llm, output=llm_name_to_cnt[output_llm], input_llms=[input_llm], output_llms=[],
+                 inputs=[llm_name_to_cnt[input_llm]])
+
+
+def parse_blocks(dot_str):
+    # dot_str = digraph.source
+
+    blocks = []
+    block_continued = []
+    for i, line in enumerate(dot_str.split('\n')):
+        if "->" in line and len(block_continued) != 0:
+            blocks.append("\n".join(block_continued))
+            block_continued = [line.strip()]
+        elif "}" not in line and line.strip() != "" and "{" not in line:
+            block_continued.append(line.strip())  # give back the line break
+
+    blocks.append("\n".join(block_continued))
+
+    constructed_blocks = []
+
+    for block in blocks:
+        # block = [input -> output, input_llm_str, output_llm_str]
+        block_rep = construct_block(block)
+        constructed_blocks.append(block_rep)
+
+    # re-assemble:
+    # rule: if input -> output, if ouput is the same, add the input to the input list (merge)
+    # the current merge is on LLM name + function name
+    # but we might want to perform this on the LLM agent level
+
+    # input -> output
+    # if the node name is in another llm's input list, then, we add that node to the output llm list
+    finished_blocks = []
+    processed_blocks = copy(constructed_blocks)
+    while len(processed_blocks) != 0:
+        block = processed_blocks.pop()
+
+        if block in finished_blocks:
+            continue
+
+        # print(f"processing {block.name}")
+        # add input llms
+        same_blocks = filter(lambda x: x == block, processed_blocks)
+        for b in same_blocks:
+            # print(b.name)
+            # print(b.input_llms)
+            block.input_llms.extend(b.input_llms)
+            block.inputs.extend(b.inputs)
+
+        # add output llms
+        outputs = filter(lambda x: block.name in x.input_llms, processed_blocks)
+        out_llm_names = [o.name for o in outputs]
+        block.output_llms.extend(out_llm_names)
+
+        finished_blocks.append(block)
+
+    return finished_blocks
+
+
+def display_full_graph(blocks):
+    assert type(blocks[0]) == Block
+    content = ""
+
+    # we traverse each block
+    # we start by nodes that have the most input llms that have no parents
+    sorting_list = []
+    block_names = [b.name for b in blocks]
+    for b in blocks:
+        cnt = 0
+        for i in b.input_llms:
+            if i in block_names:
+                cnt += 1  # it has an input LLM that's also dependent on other LLMs
+        sorting_list.append((b, cnt))
+    sorting_list = sorted(sorting_list, key=lambda x: x[1])
+    blocks = [b[0] for b in sorting_list]
+
+    # print input one by one, and then output
+    for b in blocks:
+        # input llms -> output llm
+        content += f"Inputs: {b.input_llms} -> {b.name}\n"
+        for i, iput in enumerate(b.inputs):
+            content += f"  Input {b.input_llms[i]}:\n"
+            content += f"    {iput}\n"
+
+        content += f"Output: {b.name}\n"
+        content += f"    {b.output}\n"
+
+        content += "\n"
+
+    return content
+class LLMCallable(object):
+    def __init__(self, llm, config_list, system_message, name="helper"):
+        self.llm = llm
+        self.config_list = config_list
+        self.llm = AssistantAgent(name=name,
+                                  system_message=system_message,
+                                  llm_config={"config_list": config_list})
+
+    def create_response(self, message):
+        messages = [{'content': message, 'role': 'user'}]
+        response = self.llm.client.create(messages=self.llm._oai_system_message + messages)
+        return self.llm.client.extract_text_or_completion_object(response)[0]
+
+class LLMModuleReasoner(LLMCallable):
+    def __init__(self, parameters, config_list, task_description, *args, **kwargs):
+        super().__init__(parameters, *args, **kwargs)
+        sys_msg = dedent("""
+        You are giving instructions to a student on how to accomplish a task.
+        The student aims to get a high score.
+        Given the feedback the student has received and the instruction you have given,
+        You want to come up with a new instruction that will help the student get a higher score.
+        """)
+        self.llm = AssistantAgent(name="assistant",
+                                  system_message=sys_msg,
+                                  llm_config={"config_list": config_list})
+        self.task_description = task_description
+
+        self.parameter_copy = {}
+        self.save_parameter_copy()
+
+    def _step(self, value, feedback):
+        context = [self.task_description, value['content'], feedback]
+        prompt_space = dedent("""
+        {}
+
+        This is your instruction to the student from the previous round:
+        {}
+
+        Using this instruction, this is the feedback the student received:
+        {}
+
+        Please write down a new instruction to help the student achieve a higher score.
+        Be concise and to the point.
+        Remember the student is starting from scratch, not revising their old work:
+        """.format(*context))
+
+        messages = [{'content': prompt_space, 'role': 'user'}]
+        response = self.llm.client.create(messages=self.llm._oai_system_message + messages)
+        # response = Completion.create(messages=self.llm._oai_system_message + messages)
+        # new_instruct = self.llm.client.extract_text_or_function_call(response)[0]
+        new_instruct = self.llm.client.extract_text_or_completion_object(response)[0]
+        new_node = {'content': new_instruct, 'role': 'system'}
+        return new_node
+
+
+class AgentExecutionSummary:
+    ANALYSIS_PROMPT = dedent("""
+    Considering the following task:
+
+    TASK: {task}
+
+    Here are the list of agents involved in completing the task:
+
+    AGENT LIST:
+    {agent_list}
+
+    Here is a brief summary of how these agents attempted to solve this task:
+
+    """)
+
+
+def simple_shrink(dot_str, shrink=True):
+    """
+    This provides a heuristic shrink to reduce the lines of docstring that describes the graph.
+
+    Args:s
         dot_str: the returned object from calling backward on a node with (visualize=True, reverse_plot=False)
         shrink: if set True, the dot_str will not be a valid GraphViz dot str; otherwise it will still be valid
 
@@ -209,92 +408,3 @@ def process_dot_str(dot_str, shrink=True):
     blocks = ["\n".join(b) for b in blocks]
 
     return begin_str + "\n".join(blocks) + '\n' + end_str
-
-class AgentExecutionSummary:
-    ANALYSIS_PROMPT = dedent("""
-    Considering the following task:
-
-    TASK: {task}
-
-    Here are the list of agents involved in completing the task:
-
-    AGENT LIST:
-    {agent_list}
-
-    Here is a brief summary of how these agents attempted to solve this task:
-
-    """)
-
-
-# This updates feedback before the optimizer
-class LLMOptimizationPathSummary:
-    def __init__(self, parameters, config_list, *args, **kwargs):
-        # can add task description in here
-        assert type(parameters) is list
-        assert all([isinstance(p, ParameterNode) for p in parameters])
-        self.parameters = parameters
-        sys_msg = dedent("""
-                You are analyzing the crash report of an intelligent assistant.
-                The crash report prints out the path of the execution of the assistant, but might not be helpful.
-                It also contains the feedback the assistant received from the user.
-                Give a high-level, concise summary of the crash report.
-                """)
-        self.llm = AssistantAgent(name="assistant",
-                                  system_message=sys_msg,
-                                  llm_config={"config_list": config_list})
-        self.parameter_copy = {}
-        self.save_parameter_copy()
-
-    def save_parameter_copy(self):
-        # this is to be able to go back
-        for p in self.parameters:
-            if p.trainable:
-                self.parameter_copy[p] = {"_data": copy(p._data), "_feedback": copy(p._feedback)}
-
-    def update_feedback(self):
-        # before update,  we save it once
-        self.save_parameter_copy()
-        for p in self.parameters:
-            if p.trainable:
-                p._feedback = self._update_feedback(p._feedback)  # NOTE: This is an in-place update
-
-    def _update_feedback(self, feedback):
-        new_feedback_dict = {}
-        for parent_node, list_of_feedback in feedback.items():
-            all_feedback = "\n\n".join(list_of_feedback)
-            messages = [{'content': all_feedback, 'role': 'user'}]
-            response = self.llm.client.create(messages=self.llm._oai_system_message + messages)
-            # new_feedback = self.llm.client.extract_text_or_function_call(response)[0]
-            # new_feedback = Completion.extract_text_or_function_call(response)[0]
-            new_feedback = self.llm.client.extract_text_or_completion_object(response)[0]
-            print(new_feedback)
-            new_feedback_dict[parent_node] = new_feedback
-        return new_feedback_dict
-
-class FeedbackEnhance:
-    def __init__(self, parameters, config_list, *args, **kwargs):
-        # can add task description in here
-        assert type(parameters) is list
-        assert all([isinstance(p, ParameterNode) for p in parameters])
-        self.parameters = parameters
-        sys_msg = dedent("""
-                ...
-        """)
-        self.llm = AssistantAgent(name="assistant",
-                                  system_message=sys_msg,
-                                  llm_config={"config_list": config_list})
-
-    def update_feedback(self):
-        for p in self.parameters:
-            if p.trainable:
-                p._feedback = self._update_feedback(p._feedback)  # NOTE: This is an in-place update
-
-    def _update_feedback(self, feedback):
-        new_feedback_dict = {}
-        for parent_node, list_of_feedback in feedback.items():
-            all_feedback = "\n\n".join(list_of_feedback)
-            messages = [{'content': all_feedback, 'role': 'user'}]
-            response = self.llm.client.create(messages=self.llm._oai_system_message + messages)
-            new_feedback = self.llm.client.extract_text_or_function_call(response)[0]
-            new_feedback_dict[parent_node] = new_feedback
-        return new_feedback_dict
