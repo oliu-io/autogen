e5181e1b1f2015aadd2cb8b048a494b2e86891c9
chinganc <chinganc0@gmail.com>
2024-03-06 14:31:05 -0800

    Update core.ipynb

--- autogen/trace/__init__.py
+++ autogen/trace/__init__.py
@@ -1,2 +1,2 @@
 from autogen.trace.trace import trace, node, stop_tracing, compatability
-from autogen.trace.trace_ops import trace_op, apply_op
+from autogen.trace.trace_ops import trace_op, apply_op, NodeContainer
